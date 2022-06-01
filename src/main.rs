6550e5e94f44fa9c30f8b6fa0f255868b8ccbf6d
ladihzey <yegorzhidal@gmail.com>
2022-06-01 19:33:15 +0300

    Format code

--- src/main.rs
+++
@@ -1,6 +0,0 @@
-mod sort;
-
-fn main() {
-    sort::bubble_sort::sort(&mut [1234]);
-    println!("Hello, world!");
-}
---
+++ src/sort.rs
@@ -0,0 +1,34 @@
+mod bubble_sort;
+mod selection_sort;
+
+pub use bubble_sort::sort as bubble_sort;
+pub use selection_sort::sort as selection_sort;
+
+#[cfg(test)]
+mod tests {
+    use test_case::test_case;
+    use super::*;
+
+    type SortFunction = fn(&mut [i32]) -> &[i32];
+
+    #[test_case(bubble_sort)]
+    #[test_case(selection_sort)]
+    fn should_tolerate_empty_array(sort: SortFunction) {
+        assert_eq!(sort(&mut []), []);
+    }
+
+    #[test_case(bubble_sort)]
+    #[test_case(selection_sort)]
+    fn should_tolerate_single_item_array(sort: SortFunction) {
+        assert_eq!(sort(&mut [1]), [1]);
+    }
+
+    #[test_case(bubble_sort)]
+    #[test_case(selection_sort)]
+    fn should_sort_array(sort: SortFunction) {
+        assert_eq!(
+            sort(&mut [123, 0, -100, 12, 25, 257, 1]),
+            [-100, 0, 1, 12, 25, 123, 257]
+        );
+    }
+}
