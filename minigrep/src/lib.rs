74325d2802ef312ed22457ec6a0f313fa94a5d2c
ladihzey <yegorzhidal@gmail.com>
2022-07-12 01:47:52 +0300

    Change creation of test cases assets

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -59,29 +59,31 @@ mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
-        let query = "duct";
-        let contents = "Rust:\nsafe, fast, productive.\nPick three.";
-
-        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
-    }
-
-    #[test]
     fn case_sensitive() {
         let query = "duct";
-        let contents = "Rust:\nsafe, fast, productive.\nPick three.\nDuct tape.";
+        let contents = [
+            "Rust:",
+            "safe, fast, productive.",
+            "Pick three.",
+            "Duct tape.",
+        ].join("\n");
 
-        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+        assert_eq!(vec!["safe, fast, productive."], search(query, &contents));
     }
 
     #[test]
     fn case_insensitive() {
         let query = "rUsT";
-        let contents = "Rust:\nsafe, fast, productive.\nPick three.\nTrust me.";
+        let contents = [
+            "Rust:",
+            "safe, fast, productive.",
+            "Pick three.",
+            "Trust me.",
+        ].join("\n");
 
         assert_eq!(
             vec!["Rust:", "Trust me."],
-            search_case_insensitive(query, contents)
+            search_case_insensitive(query, &contents)
         );
     }
 }
